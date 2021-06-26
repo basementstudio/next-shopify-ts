1e9a15f31d54b3b4973961343236c48352619244
Julian Benegas <julianbenegas99@gmail.com>
2021-06-26 16:23:52 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import PageLayout from 'components/layout/page'
+import Button from 'components/primitives/button'
+
+const HomePage = () => {
+  return (
+    <PageLayout>
+      <Button>Hola!</Button>
+    </PageLayout>
+  )
+}
+
+export default HomePage
